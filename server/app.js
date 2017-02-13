2e153467a9d56fbff353ae8087edc188a2849c4a
Mikey Badr <mikey.badr@attck.com>
2017-02-13 13:40:08 -0500

    init server and template

---
+++ server/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var morgan = require('morgan');
+var port = process.env.PORT || 8080;
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+app.route('/')
+  .get(function(req, res){
+      res.render('index');
+  });
+
+app.listen(port, function(){
+    console.log('Node listening on port ' + port);
+});
+
