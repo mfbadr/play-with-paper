1b5c57c5720c043be612729bd227f802646fe32e
Mikey Badr <mikey.badr@attck.com>
2017-02-13 16:02:26 -0500

    add css and style loaders, paper js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,18 @@
-alert('foo');
+'use strict';
+// alert('foo');
+
+import paper from '../../node_modules/paper/dist/paper-full.min.js';
+import styles from '../css/style.css';
+
+paper.install(window);
+window.onload = function() {
+  // Setup directly from canvas id:
+  paper.setup('canvas');
+  var path = new Path();
+  path.strokeColor = 'white';
+  var start = new Point(100, 100);
+  path.moveTo(start);
+  path.lineTo(start.add([ 200, -50 ]));
+  view.draw();
+}
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,25 @@
+var path = require('path');
 module.exports = {
 	entry: "./src/js/index.js",
 	output: {
-		path: __dirname + '/dist/',
+		path: path.resolve(__dirname, 'dist'),
 		filename: "bundle.js"
-	}
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.json$/,
+				loader: "json-loader"
+			},
+			{
+				test: /\.node$/,
+				loader: "node-loader"
+			},
+			{
+				test: /\.css/,
+				loaders: ['style-loader', 'css-loader'],
+				include: __dirname + '/src'
+			}
+	]
+  },
 };
